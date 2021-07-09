ec9baf61aa24ad9a8cf229ac89fd131bfdf5d0de
MurrayWarnock <murray.warnock@gmail.com>
2021-07-09 09:53:20 -0600

    Added state initialization in PlantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -7,7 +7,10 @@ export default class PlantList extends Component {
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
-
+  state = {
+    plants: [],
+  }
+  
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
     .then(res => {
